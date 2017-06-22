cbc2f8d7b3cd452f2a16da8cb857016ab4ba8901
sershishkov <ser.shishkov@gmail.com>
2017-06-22 15:08:08 +0300

    Other accordion

--- JS/jS_Lesson_19_20/js/src/script_1.js
+++ JS/jS_Lesson_19_20/js/src/script_1.js
@@ -3,21 +3,9 @@
 
 $(function() {
     $('.jcarousel').jcarousel();
-////////////////////////////////////////////////////////////////////////////
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].onclick = function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight){
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-        }
-    }
+    $( "#accordion" ).accordion({
+        collapsible: true
+    });
 
     //////////////////////////////////////////////////
    var myData =  [
